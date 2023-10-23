b6ed484a6a88b26451592a7b29f53fade6736638
Noe Magne <Noeca@HARLE_KING.com>
2023-10-23 08:47:45 -0400

    Ejercicio3

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double num1, num2;
+
+        Console.WriteLine("Ingrese el primer número: ");
+        if (double.TryParse(Console.ReadLine(), out num1))
+        {
+            Console.WriteLine("Ingrese el segundo número: ");
+            if (double.TryParse(Console.ReadLine(), out num2))
+            {
+                double mayor = EncontrarMayor(num1, num2);
+                Console.WriteLine($"El número mayor es: {mayor}");
+            }
+            else
+            {
+                Console.WriteLine("El segundo número no es válido.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("El primer número no es válido.");
+        }
+    }
+
+    static double EncontrarMayor(double a, double b)
+    {
+        return Math.Max(a, b);
+    }
+}
